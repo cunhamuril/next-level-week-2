b1ae7e708add32d077a3c9ee03f80887f433974b
Murilo Cunha <murilo.sant@hotmail.com>
2020-08-04 18:11:49 -0300

    feat(server): add route to create classes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,11 @@
 import express from "express";
 
+import routes from "./routes";
+
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
