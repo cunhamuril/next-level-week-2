971c1ea439dcd28febc718832ed0e0eeecceaab9
Murilo Cunha <murilo.sant@hotmail.com>
2020-08-03 21:49:37 -0300

    feat(web): add TeacherItem component on TeacherList page

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars1.githubusercontent.com/u/49242233?s=460&u=810661d1c4c84630b5bc90626133ce750392d63e&v=4"
+          alt="Murilo Cunha"
+        />
+
+        <div>
+          <strong>Murilo Cunha</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /> <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+
+        <button type="button">
+          <img src={whatsappIcon} alt="Entrar em contato" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
