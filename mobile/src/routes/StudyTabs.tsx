1d7a835805b4b5ebfe2b0e835039a33a3199ce5f
Murilo Cunha <murilo.sant@hotmail.com>
2020-08-08 16:07:42 -0300

    feat(mobile): add TeacherList page

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -46,8 +46,12 @@ function StudyTabs() {
         component={TeacherList}
         options={{
           tabBarLabel: "Proffys",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="ios-easel" size={size} color={color} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name="ios-easel"
+              size={size}
+              color={focused ? "#8257e5" : color}
+            />
           ),
         }}
       />
@@ -56,8 +60,12 @@ function StudyTabs() {
         component={Favorites}
         options={{
           tabBarLabel: "Favoritos",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="ios-heart" size={size} color={color} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name="ios-heart"
+              size={size}
+              color={focused ? "#8257e5" : color}
+            />
           ),
         }}
       />
